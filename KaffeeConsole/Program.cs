aad5c71cf7aeb5e25990cad6c8ee3d9598bc536b
Andreas Gropp <starmaerker@googlemail.com>
2018-04-09 16:01:54 +0200

    first commit

---
+++ KaffeeConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffeeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (args.Length >= 2)
+            {
+                Console.WriteLine(args[0] + " " + args[1]); 
+            }
+            else
+            {
+                Console.WriteLine("Hallo Welt");
+            }
+            
+            Console.WriteLine();
+            Console.Write("press any key to quit: ");
+            Console.ReadKey();            
+            
+        }
+        
+    }
+}
---
+++ KaffeeModell/Behaelter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffeeModell
+{
+    public class Behaelter
+    {
+        #region Klassenvariablen (Felder)
+
+        public uint _volumen;
+
+        public uint _fuellstand;
+
+
+        #endregion
+
+        #region Methoden
+
+        /// <summary>
+        /// etwas in Behälter einfüllen
+        /// </summary>
+        /// <param name="menge">die einzufüllende Menge</param>
+        /// <returns>die tatsächlich eingefüllte Menge</returns>
+
+        public uint Fuellen(uint menge)
+        {
+            if (menge < _volumen - _fuellstand)
+            {
+                _fuellstand += menge;
+                return menge;
+            }
+            else
+            {
+                uint eingefuellt = _volumen - _fuellstand;
+                _fuellstand = _volumen;
+                return eingefuellt;
+            }
+        }
+
+        /// <summary>
+        /// Füllt den Behälter bis zum Rand
+        /// </summary>
+        /// <returns>die tatsächlich eingefüllte Menge</returns>
+        public uint Fuellen()
+        {
+            return Fuellen(_volumen - _fuellstand);
+        }
+           
+
+        #endregion
+    }
+}
